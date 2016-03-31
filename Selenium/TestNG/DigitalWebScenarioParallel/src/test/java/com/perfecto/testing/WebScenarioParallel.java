7514f7e1817b30799ecb904066df3cb860a9725e
Asaf Saar <saarasaf@gmail.com>
2016-03-31 22:14:48 +0300

    TestNG sample of digital scenario, in parallel

---
+++ Selenium/TestNG/DigitalWebScenarioParallel/src/test/java/com/perfecto/testing/WebScenarioParallel.java
@@ -0,0 +1,102 @@
+package com.perfecto.testing;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.Select;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.perfecto.testing.utils.Utils;
+
+
+public class WebScenarioParallel {
+	RemoteWebDriver driver;
+
+	
+	@Parameters({ "platformName", "platformVersion", "browserName", "browserVersion", "deviceId" })
+	@BeforeTest
+	public void beforeTest(String platformName, String platformVersion, String browserName, String browserVersion, String deviceId) throws MalformedURLException {
+		driver = Utils.getRemoteWebDriver(platformName, platformVersion, browserName, browserVersion, deviceId);
+	}
+	
+	@Test
+	public void searchGoogle() throws MalformedURLException {				
+
+		driver.get("http://www.google.com");
+
+		try {
+			final String searchKey = "Perfecto Mobile";
+			WebElement element = driver.findElement(By.name("q"));
+			element.sendKeys(searchKey);
+			element.submit();
+			(new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+				public Boolean apply(WebDriver d) {
+					return d.getTitle().toLowerCase().startsWith(searchKey.toLowerCase());
+				}
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Done searchGoogle\n");
+	}
+
+	@Test
+	public void geicoInsurance() throws MalformedURLException {
+
+		driver.get("http://www.geico.com");
+
+		try {
+
+			Select type = new Select(driver.findElement(By.id("insurancetype")));
+
+			type.selectByVisibleText("Motorcycle");
+			driver.findElement(By.id("zip")).sendKeys("01434");
+			driver.findElement(By.id("submitButton")).click();
+
+			driver.findElement(By.id("firstName")).sendKeys("MyFirstName");
+			driver.findElement(By.id("lastName")).sendKeys("MyFamilyName");
+			driver.findElement(By.id("street")).sendKeys("My Address");
+
+			driver.findElement(By.id("date-monthdob")).sendKeys("8");
+			driver.findElement(By.id("date-daydob")).sendKeys("3");
+			driver.findElement(By.id("date-yeardob")).sendKeys("1981");
+
+			driver.findElement(By.xpath("//*[@class='radio'][2]")).click();
+			driver.findElement(By.id("btnSubmit")).click();
+
+			driver.findElement(By.id("hasCycle-error")).isDisplayed();
+
+			Select hasCycle = new Select(driver.findElement(By.id("hasCycle")));
+			hasCycle.selectByIndex(1);
+
+			Select current = new Select(driver.findElement(By.id("currentInsurance")));
+			current.selectByVisibleText("Other");
+			driver.findElement(By.id("btnSubmit")).click();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Done geicoInsurance\n");
+	}
+
+	@Parameters({ "deviceId" })
+	@AfterTest
+	public void afterTest(String deviceId) throws IOException {
+		driver.close();
+		driver.quit();	
+	}	
+	
+	
+	
+}
