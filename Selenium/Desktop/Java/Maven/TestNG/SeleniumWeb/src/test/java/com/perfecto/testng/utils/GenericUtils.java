37926c9ea1c5ff250ab9f8e2904365b854d166a5
Asaf Saar <asaf.saar@yahoo.com>
2016-04-01 18:15:00 +0300

    Update GenericUtils.java

--- Selenium/Desktop/Java/Maven/TestNG/SeleniumWeb/src/test/java/com/perfecto/testng/utils/GenericUtils.java
+++ Selenium/Desktop/Java/Maven/TestNG/SeleniumWeb/src/test/java/com/perfecto/testng/utils/GenericUtils.java
@@ -6,13 +6,13 @@ import java.util.Date;
 public class GenericUtils {
 
 	// TODO: Set your cloud host and credentials
-	public static final String HOST = "https://web-demo.perfectomobile.com/nexperience/perfectomobile/wd/hub";
-	public static final String USER = "asafs@perfectomobile.com";
-	public static final String PASSWORD = "Spike123!";
-	public static final String DEVICENAME = "1015FA089D682305";
+	public static final String HOST = "https://MY_HOST.perfectomobile.com/nexperience/perfectomobile/wd/hub";
+	public static final String USER = "MY_USER";
+	public static final String PASSWORD = "MY_PASSWORD";
+	public static final String DEVICENAME = "MY_DEVICE_ID";
 
 	// TODO: Set your reports folder path
-	public static final String REPORT_PATH = "C:\\Development\\Reports\\";
+	public static final String REPORT_PATH = "C:/Development/Reports/";
 
 	public static String getCurrentTime() {
 		return new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date());
