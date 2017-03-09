744b618f4a64f349de90fbad663f0ff7a0b3824f
Erez Cohen <erez.cohen@gmail.com>
2017-03-09 13:33:01 +0200

    Refactor + add example for artifacts download

--- Reporting/Java/export-api-sample/src/main/java/com/perfecto/reporting/sample/api/Main.java
+++ Reporting/Java/export-api-sample/src/main/java/com/perfecto/reporting/sample/api/Main.java
@@ -1,8 +1,6 @@
 package com.perfecto.reporting.sample.api;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -17,97 +15,196 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
-import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
+    // The Perfecto Continuous Quality Lab you work with
     public static final String CQL_NAME = "demo";
 
-    // Use this for US
+    // The reporting Server address depends on the location of the lab. Please refer to the documentation at
+    // http://developers.perfectomobile.com/display/PD/Reporting#Reporting-ReportingserverAccessingthereports to find your relevant address
+    // For example the following is used for US:
     public static final String REPORTING_SERVER_URL = "https://" + CQL_NAME + ".reporting-01.perfectomobile.com";
 
-    // Use this for Europe
-    // public static final String REPORTING_SERVER_URL = "https://" + CQL_NAME + ".reporting-12.perfectomobile.com";
-
-    public static final String OFFLINE_TOKEN = "MY_CQ_LAB_OFFLINE_TOKEN";
+    // See http://developers.perfectomobile.com/display/PD/Using+the+Reporting+Public+API on how to obtain an Offline Token
+    public static final String OFFLINE_TOKEN = "MY_CONTINUOUS_QUALITY_LAB_OFFLINE_TOKEN";
 
     public static final String CQL_SERVER_URL = "https://" + CQL_NAME + ".perfectomobile.com";
 
+
     public static void main(String[] args) throws Exception {
-        HttpClient httpClient = HttpClientBuilder.create().build();
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String accessToken;
+        // Use your personal offline token to obtain an access token
+        String accessToken = obtainAccessToken();
 
-        // Use your personal offline token to retrieve an access token
-        HttpPost tokenPost = new HttpPost(new URI(CQL_SERVER_URL + "/services/v2.0/auth/access-token"));
-        tokenPost.setEntity(new UrlEncodedFormEntity(Collections.singletonList(new BasicNameValuePair("offline_token", OFFLINE_TOKEN))));
-        System.out.println(tokenPost);
-        HttpResponse tokenResponse = httpClient.execute(tokenPost);
-        JsonObject tokenEntity = gson.fromJson(new InputStreamReader(tokenResponse.getEntity().getContent()), JsonObject.class);
-        accessToken = tokenEntity.getAsJsonObject("data").get("access_token").getAsString();
-        System.out.println("Got access token:\n" + accessToken);
+        // Retrieve a list of the test executions in your lab (as a json)
+        JsonObject executions = retrieveTestExecutions(accessToken);
 
-        // Retrieves a list of the test executions in your lab for the past month
+        JsonObject testExecution = executions.getAsJsonArray("resources").get(0).getAsJsonObject();
+        String testId = testExecution.get("id").getAsString();
+        String driverExecutionId = testExecution.get("externalId").getAsString();
+
+        // Retrieves a list of commands of a single test (as a json)
+        retrieveTestCommands(testId, accessToken);
+
+        // Download an execution summary PDF report of an execution (may contain several tests)
+        downloadExecutionSummaryReport(driverExecutionId, accessToken);
+
+        // Download a PDF report of a single test
+        downloadTestReport(testId, accessToken);
+
+        // Download video
+        downloadVideo(testExecution);
+
+        // Download attachments such as device logs, vitals or network files (relevant for Mobile tests only)
+        downloadAttachments(testExecution);
+    }
+
+
+    private static JsonObject retrieveTestExecutions(String accessToken)
+            throws URISyntaxException, IOException {
         URIBuilder uriBuilder = new URIBuilder(REPORTING_SERVER_URL + "/export/api/v1/test-executions");
-        uriBuilder.addParameter("startExecutionTime[0]", Long.toString(System.currentTimeMillis() - 30 * 24 * 60 * 60 * 1000L));
+
+        // Optional: Filter by range. In this example: retrieve test executions of the past month (result may contain tests of multiple driver executions)
+        uriBuilder.addParameter("startExecutionTime[0]", Long.toString(System.currentTimeMillis() - TimeUnit.DAYS.toMillis(30)));
         uriBuilder.addParameter("endExecutionTime[0]", Long.toString(System.currentTimeMillis()));
+
+        // Optional: Filter by a specific driver execution ID that you can obtain at script execution
+        // uriBuilder.addParameter("externalId[0]", "SOME_DRIVER_EXECUTION_ID");
+
         HttpGet getExecutions = new HttpGet(uriBuilder.build());
         addDefaultRequestHeaders(getExecutions, accessToken);
-        System.out.println(getExecutions);
+        HttpClient httpClient = HttpClientBuilder.create().build();
+
         HttpResponse getExecutionsResponse = httpClient.execute(getExecutions);
-        JsonObject executionsEntity = gson.fromJson(new InputStreamReader(getExecutionsResponse.getEntity().getContent()), JsonObject.class);
-        System.out.println("List of test executions response:\n" + gson.toJson(executionsEntity));
+        JsonObject executions;
+        try (InputStreamReader inputStreamReader = new InputStreamReader(getExecutionsResponse.getEntity().getContent())) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            executions = gson.fromJson(IOUtils.toString(inputStreamReader), JsonObject.class);
+            System.out.println("\nList of test executions response:\n" + gson.toJson(executions));
+        }
+        return executions;
+    }
 
-        String testId = executionsEntity.getAsJsonArray("resources").get(0).getAsJsonObject().get("id").getAsString();
-        String driverExecutionId = executionsEntity.getAsJsonArray("resources").get(0).getAsJsonObject().get("externalId").getAsString();
-        int timeZoneOffsetMinutes = TimeZone.getDefault().getOffset(System.currentTimeMillis()) / (60 * 1000);
-
-        // Retrieves a list of commands of a test
+    private static void retrieveTestCommands(String testId, String accessToken)
+            throws URISyntaxException, IOException {
         HttpGet getCommands = new HttpGet(new URI(REPORTING_SERVER_URL + "/export/api/v1/test-executions/" + testId + "/commands"));
         addDefaultRequestHeaders(getCommands, accessToken);
-        System.out.println(getCommands);
+        HttpClient httpClient = HttpClientBuilder.create().build();
         HttpResponse getCommandsResponse = httpClient.execute(getCommands);
-        JsonObject commandsEntity = gson.fromJson(new InputStreamReader(getCommandsResponse.getEntity().getContent()), JsonObject.class);
-        System.out.println("List of commands response:\n" + gson.toJson(commandsEntity));
-
-        // Retrieves summary PDF report of an execution (can contain several tests)
-        uriBuilder = new URIBuilder(REPORTING_SERVER_URL + "/export/api/v1/test-executions/pdf");
-        uriBuilder.addParameter("externalId[0]", driverExecutionId);
-        uriBuilder.addParameter("timeZoneOffsetMinutes", Integer.toString(timeZoneOffsetMinutes));
-        HttpGet getSummaryPdf = new HttpGet(uriBuilder.build());
-        addDefaultRequestHeaders(getSummaryPdf, accessToken);
-        System.out.println(getSummaryPdf);
-        HttpResponse getSummaryPdfResponse = httpClient.execute(getSummaryPdf);
-        if (HttpStatus.SC_OK == getSummaryPdfResponse.getStatusLine().getStatusCode()) {
-            Path summaryPdfFile = Files.createTempFile(driverExecutionId, ".pdf");
-            IOUtils.copy(getSummaryPdfResponse.getEntity().getContent(), new FileOutputStream(summaryPdfFile.toFile()));
-            System.out.println("Saved execution summary PDF report to: " + summaryPdfFile.toFile().getAbsolutePath());
-        } else {
-            EntityUtils.consumeQuietly(getSummaryPdfResponse.getEntity());
-        }
-
-        // Retrieves PDF report of a test
-        uriBuilder = new URIBuilder(REPORTING_SERVER_URL + "/export/api/v1/test-executions/pdf/" + testId);
-        uriBuilder.addParameter("timeZoneOffsetMinutes", Integer.toString(timeZoneOffsetMinutes));
-        HttpGet getPdf = new HttpGet(uriBuilder.build());
-        addDefaultRequestHeaders(getPdf, accessToken);
-        System.out.println(getPdf);
-        HttpResponse getPdfResponse = httpClient.execute(getPdf);
-        if (HttpStatus.SC_OK == getPdfResponse.getStatusLine().getStatusCode()) {
-            Path pdfFile = Files.createTempFile(testId, ".pdf");
-            IOUtils.copy(getPdfResponse.getEntity().getContent(), new FileOutputStream(pdfFile.toFile()));
-            System.out.println("Saved test PDF report to: " + pdfFile.toFile().getAbsolutePath());
-        } else {
-            EntityUtils.consumeQuietly(getPdfResponse.getEntity());
+        try (InputStreamReader inputStreamReader = new InputStreamReader(getCommandsResponse.getEntity().getContent())) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            JsonObject commands = gson.fromJson(IOUtils.toString(inputStreamReader), JsonObject.class);
+            System.out.println("\nList of commands response:\n" + gson.toJson(commands));
         }
     }
 
-    public static void addDefaultRequestHeaders(HttpRequestBase request, String accessToken) {
+    private static void downloadExecutionSummaryReport(String driverExecutionId, String accessToken)
+            throws URISyntaxException, IOException {
+        URIBuilder uriBuilder = new URIBuilder(REPORTING_SERVER_URL + "/export/api/v1/test-executions/pdf");
+        uriBuilder.addParameter("externalId[0]", driverExecutionId);
+        downloadFileAuthenticated(driverExecutionId, uriBuilder.build(), ".pdf", "execution summary PDF report", accessToken);
+    }
+
+    private static void downloadTestReport(String testId, String accessToken)
+            throws URISyntaxException, IOException {
+        URIBuilder uriBuilder = new URIBuilder(REPORTING_SERVER_URL + "/export/api/v1/test-executions/pdf/" + testId);
+        downloadFileAuthenticated(testId, uriBuilder.build(), ".pdf", "test PDF report", accessToken);
+    }
+
+    private static void downloadVideo(JsonObject testExecution)
+            throws IOException, URISyntaxException {
+        JsonArray videos = testExecution.getAsJsonArray("videos");
+        if (videos.size() > 0) {
+            JsonObject video = videos.get(0).getAsJsonObject();
+            String downloadVideoUrl = video.get("downloadUrl").getAsString();
+            String format = "." + video.get("format").getAsString();
+            String testId = testExecution.get("id").getAsString();
+            downloadFile(testId, URI.create(downloadVideoUrl), format, "video");
+        } else {
+            System.out.println("\nNo videos found for test execution");
+        }
+    }
+
+    private static void downloadAttachments(JsonObject testExecution)
+            throws IOException, URISyntaxException {
+        // Example for downloading device logs
+        JsonArray artifacts = testExecution.getAsJsonArray("artifacts");
+        for (JsonElement artifactElement : artifacts) {
+            JsonObject artifact = artifactElement.getAsJsonObject();
+            String artifactType = artifact.get("type").getAsString();
+            if (artifactType.equals("DEVICE_LOGS")) {
+                String testId = testExecution.get("id").getAsString();
+                String path = artifact.get("path").getAsString();
+                URIBuilder uriBuilder = new URIBuilder(path);
+                downloadFile(testId, uriBuilder.build(), ".zip", "device logs");
+            }
+        }
+    }
+
+
+    // Utils
+
+    private static String obtainAccessToken() throws URISyntaxException, IOException {
+        HttpPost tokenPost = new HttpPost(new URI(CQL_SERVER_URL + "/services/v2.0/auth/access-token"));
+        tokenPost.setEntity(new UrlEncodedFormEntity(Collections.singletonList(new BasicNameValuePair("offline_token", OFFLINE_TOKEN))));
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpResponse tokenResponse = httpClient.execute(tokenPost);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String accessToken;
+        try (InputStreamReader inputStreamReader = new InputStreamReader(tokenResponse.getEntity().getContent())) {
+            JsonObject tokenEntity = gson.fromJson(inputStreamReader, JsonObject.class);
+            accessToken = tokenEntity.getAsJsonObject("data").get("access_token").getAsString();
+            System.out.println("Got access token:\n" + accessToken);
+        }
+        return accessToken;
+    }
+
+    private static void downloadFile(String fileName, URI uri, String suffix, String description)
+            throws IOException {
+        downloadFileToFS(new HttpGet(uri), fileName, suffix, description);
+    }
+
+    private static void downloadFileAuthenticated(String fileName, URI uri, String suffix, String description, String accessToken)
+            throws IOException {
+        HttpGet httpGet = new HttpGet(uri);
+        addDefaultRequestHeaders(httpGet, accessToken);
+        downloadFileToFS(httpGet, fileName, suffix, description);
+    }
+
+    private static void downloadFileToFS(HttpGet httpGet, String fileName, String suffix, String description)
+            throws IOException {
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpResponse response = httpClient.execute(httpGet);
+        FileOutputStream fileOutputStream = null;
+        try {
+            if (HttpStatus.SC_OK == response.getStatusLine().getStatusCode()) {
+                Path file = Files.createTempFile(fileName, suffix);
+                fileOutputStream = new FileOutputStream(file.toFile());
+                IOUtils.copy(response.getEntity().getContent(), fileOutputStream);
+                System.out.println("\nSaved " + description + " to: " + file.toFile().getAbsolutePath());
+            } else {
+                String errorMsg = IOUtils.toString(response.getEntity().getContent(), Charset.defaultCharset());
+                System.err.println("Error downloading file. Status: " + response.getStatusLine() + ".\nInfo: " + errorMsg);
+            }
+        } finally {
+            EntityUtils.consumeQuietly(response.getEntity());
+            IOUtils.closeQuietly(fileOutputStream);
+        }
+
+    }
+
+    private static void addDefaultRequestHeaders(HttpRequestBase request, String accessToken) {
         request.addHeader("TENANTID", CQL_NAME);
         request.addHeader("Authorization", "Bearer " + accessToken);
     }
+
 }
+
